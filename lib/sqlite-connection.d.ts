7ac3bfc79f5c57d48d8d2747e509afb0e4c5e989
Wyatt Greenway <th317erd@gmail.com>
2022-09-14 17:12:53 -0700

    Update mythix dependencies. Fix type definitions.

--- lib/sqlite-connection.d.ts
+++ lib/sqlite-connection.d.ts
@@ -4,8 +4,8 @@ import { GenericObject } from "mythix-orm/lib/interfaces/common";
 
 declare class SQLiteConnection extends SQLConnectionBase {
   public formatResultsResponse(sqlStatement: string, columns: Array<GenericObject>, result: Array<any>): { rows: Array<any>, columns: Array<string> };
-  public getEmulatedAutoIncrementID(Model: ModelClass, field: Field): number;
-  public resetEmulatedAutoIncrementID(Model: ModelClass, field: Field): void;
+  public getEmulatedAutoIncrementID(Model: ModelClass, field?: Field): number;
+  public resetEmulatedAutoIncrementID(Model: ModelClass, field?: Field): void;
   public pragma(sql: string): Promise<any>;
 
   declare public emulatedAutoIncrementIDs: Map<ModelClass, Map<Field, number>>;
